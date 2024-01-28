ac94c61ce85f9fffde81134a219f1117b98acf85
gaowei <real.gaowei@foxmail.com>
2024-01-28 16:23:30 +0800

    调整组件位置

--- src/main/java/org/levigao/serialport/ui/HomeFrame.java
+++ src/main/java/org/levigao/serialport/ui/HomeFrame.java
@@ -83,27 +83,27 @@ public class HomeFrame extends JFrame {
         mTestDataLabel.setForeground(Color.BLACK);
         //mTestDataLabel.setText();
         //x越大越向右，y越大越下
-        mTestDataLabel.setBounds(5, 30,
+        mTestDataLabel.setBounds(20, 30,
                 UIConfig.CHINESE_CHARACTER_WIDTH*4,
                 UIConfig.CHINESE_CHARACTER_WIDTH);
         mTestDataLabel.setFont(UIConfig.TEXT_FONT);
-        mTestDataLabel.setBackground(Color.RED);
+       // mTestDataLabel.setBackground(Color.lightGray);
         mTestDataLabel.setOpaque(true);
         mHomePanel.add(mTestDataLabel);
 
 
-        mDatePicker.setBounds(5+UIConfig.CHINESE_CHARACTER_WIDTH*5,25,
+        mDatePicker.setBounds(20+UIConfig.CHINESE_CHARACTER_WIDTH*5,25,
                 UIConfig.CHINESE_CHARACTER_WIDTH*10,
                 UIConfig.CHINESE_CHARACTER_WIDTH+10);
         mHomePanel.add(mDatePicker);
 
         //第二行第一个组件
         mMotorLabel.setForeground(Color.BLACK);
-        mMotorLabel.setBounds(5, 30+UIConfig.CHINESE_CHARACTER_WIDTH*3,
+        mMotorLabel.setBounds(20, 30+UIConfig.CHINESE_CHARACTER_WIDTH*3,
                 UIConfig.CHINESE_CHARACTER_WIDTH*4,
                 UIConfig.CHINESE_CHARACTER_WIDTH);
         mMotorLabel.setFont(UIConfig.TEXT_FONT);
-        mMotorLabel.setBackground(Color.RED);
+       // mMotorLabel.setBackground(Color.lightGray);
         mMotorLabel.setOpaque(true);
         mHomePanel.add(mMotorLabel);
 
@@ -116,16 +116,16 @@ public class HomeFrame extends JFrame {
 
        //第三行第一个组件
         mReductionRateLabel.setForeground(Color.BLACK);
-        mReductionRateLabel.setBounds(5, 30+UIConfig.CHINESE_CHARACTER_WIDTH*6,
+        mReductionRateLabel.setBounds(20, 30+UIConfig.CHINESE_CHARACTER_WIDTH*6,
                 UIConfig.CHINESE_CHARACTER_WIDTH*4,
                 UIConfig.CHINESE_CHARACTER_WIDTH);
         mReductionRateLabel.setFont(UIConfig.TEXT_FONT);
-        mReductionRateLabel.setBackground(Color.RED);
+       // mReductionRateLabel.setBackground(Color.lightGray);
         mReductionRateLabel.setOpaque(true);
         mHomePanel.add(mReductionRateLabel);
 
 
-        mReductionRateTextArea.setBounds(5+UIConfig.CHINESE_CHARACTER_WIDTH*5,
+        mReductionRateTextArea.setBounds(20+UIConfig.CHINESE_CHARACTER_WIDTH*5,
                 25+UIConfig.CHINESE_CHARACTER_WIDTH*6,
                 UIConfig.CHINESE_CHARACTER_WIDTH*10,
                 UIConfig.CHINESE_CHARACTER_WIDTH+10);
@@ -136,41 +136,41 @@ public class HomeFrame extends JFrame {
 
         //第四行第一个组件
         mScaleLabel.setForeground(Color.BLACK);
-        mScaleLabel.setBounds(5, 30+UIConfig.CHINESE_CHARACTER_WIDTH*9,
+        mScaleLabel.setBounds(20, 30+UIConfig.CHINESE_CHARACTER_WIDTH*9,
                 UIConfig.CHINESE_CHARACTER_WIDTH*4,
                 UIConfig.CHINESE_CHARACTER_WIDTH);
         mScaleLabel.setFont(UIConfig.TEXT_FONT);
-        mScaleLabel.setBackground(Color.RED);
+       // mScaleLabel.setBackground(Color.lightGray);
         mScaleLabel.setOpaque(true);
         mHomePanel.add(mScaleLabel);
 
         //第五章第一个组件
         mPumpTypeLabel.setForeground(Color.BLACK);
-        mPumpTypeLabel.setBounds(5, 30+UIConfig.CHINESE_CHARACTER_WIDTH*12,
+        mPumpTypeLabel.setBounds(20, 30+UIConfig.CHINESE_CHARACTER_WIDTH*12,
                 UIConfig.CHINESE_CHARACTER_WIDTH*4,
                 UIConfig.CHINESE_CHARACTER_WIDTH);
         mPumpTypeLabel.setFont(UIConfig.TEXT_FONT);
-        mPumpTypeLabel.setBackground(Color.RED);
+       // mPumpTypeLabel.setBackground(Color.lightGray);
         mPumpTypeLabel.setOpaque(true);
         mHomePanel.add(mPumpTypeLabel);
 
         //第六行第一个组件
         mProductNo.setForeground(Color.BLACK);
-        mProductNo.setBounds(5, 30+UIConfig.CHINESE_CHARACTER_WIDTH*15,
+        mProductNo.setBounds(20, 30+UIConfig.CHINESE_CHARACTER_WIDTH*15,
                 UIConfig.CHINESE_CHARACTER_WIDTH*4,
                 UIConfig.CHINESE_CHARACTER_WIDTH);
         mProductNo.setFont(UIConfig.TEXT_FONT);
-        mProductNo.setBackground(Color.RED);
+       // mProductNo.setBackground(Color.lightGray);
         mProductNo.setOpaque(true);
         mHomePanel.add(mProductNo);
 
         //第七行第一个组件
         mClientName.setForeground(Color.BLACK);
-        mClientName.setBounds(5, 30+UIConfig.CHINESE_CHARACTER_WIDTH*18,
+        mClientName.setBounds(20, 30+UIConfig.CHINESE_CHARACTER_WIDTH*18,
                 UIConfig.CHINESE_CHARACTER_WIDTH*4,
                 UIConfig.CHINESE_CHARACTER_WIDTH);
         mClientName.setFont(UIConfig.TEXT_FONT);
-        mClientName.setBackground(Color.RED);
+        //mClientName.setBackground(Color.lightGray);
         mClientName.setOpaque(true);
         mHomePanel.add(mClientName);
 
